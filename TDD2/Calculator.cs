bac73b49a3f828186eb36ad61c91b0e8e31f8b25
piotr_daniluk <piotr.daniluk@autonomik.pl>
2021-03-16 13:55:24 +0100

    my project

---
+++ TDD2/Calculator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD2
+{
+    public class Calculator
+    {
+       
+            public static int Calculate(string input)
+            {
+                if (input == "")
+                    return 0;
+
+                input = input.TrimStart('#');
+
+                var singleNumber = GetSingleNumber(input);
+                if (singleNumber != null)
+                    return singleNumber.Value;
+
+                var twoNumbersCommaDelim = input.Split(',');
+                var twoCommaDelimSum = GetSumOfTwoNumber(twoNumbersCommaDelim);
+                if (twoCommaDelimSum != null)
+                    return twoCommaDelimSum.Value;
+
+                var twoNumbersNewLineDelim = input.Split('\n');
+                var twoNewLineDelimSum = GetSumOfTwoNumber(twoNumbersNewLineDelim);
+                if (twoNewLineDelimSum != null)
+                    return twoNewLineDelimSum.Value;
+
+                var threeNumbers = input.Split('\n', ',');
+                var threeNumbersSum = GetSumOfThreeNumber(threeNumbers);
+                if (threeNumbersSum != null)
+                    return threeNumbersSum.Value;
+
+                return int.MinValue;
+            }
+
+            private static int? GetSingleNumber(string input)
+            {
+                var isSingleNumber = Int32.TryParse(input, out int result);
+                if (isSingleNumber)
+                {
+                    if (result < 0)
+                        throw new ArgumentException();
+                    else if (result > 1000)
+                        return 0;
+                    return result;
+                }
+                return null;
+            }
+
+            private static int? GetSumOfTwoNumber(string[] twoNumbers)
+            {
+                if (twoNumbers.Length == 2)
+                {
+                    var firstNum = GetSingleNumber(twoNumbers[0]);
+                    var secondNum = GetSingleNumber(twoNumbers[1]);
+                    if (firstNum != null && secondNum != null)
+                        return firstNum.Value + secondNum.Value;
+                }
+                return null;
+            }
+
+            private static int? GetSumOfThreeNumber(string[] threeNumbers)
+            {
+                if (threeNumbers.Length == 3)
+                {
+                    var firstSum = GetSumOfTwoNumber(new string[] { threeNumbers[0], threeNumbers[1] });
+                    var lastNumber = GetSingleNumber(threeNumbers[2]);
+                    if (firstSum != null && lastNumber != null)
+                        return firstSum.Value + lastNumber.Value;
+                }
+                return null;
+            }
+
+        }
+    }
+
+
---
+++ TDD2Testy/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDD2Testy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDD2Testy")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37c2d004-2034-47f2-a1cb-fac9c409be4e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
